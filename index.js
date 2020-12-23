cfced2f21e334287a7e432118263f0b18e16c83a
Evgeniya Kirtoka <evgeniyakirtoka@gmail.com>
2020-12-23 20:43:09 +0200

    Add 1, 2 tasks

--- index.js
+++ index.js
@@ -1,85 +1,65 @@
-const num1 = + prompt('Введите первое число:');
-const num2 = + prompt('Введите второе число:');
-const sign = prompt('Введите знак (+, -, *, /):');
+// const GOOD_PASSWORD = 'qwerty123';
+
+// function whiteRightPassword() {
+//   let count = 0;
+//   while (count < 5) {
+//     count++;
+//     const userInput = prompt ('Введите пароль:');
+//     if(userInput === GOOD_PASSWORD) {
+//       break;
+//     }
+//   }
+// }
+// whiteRightPassword();
 
 
-const calc = function (num1, num2, sign) {
-  if (isNaN(num1) || isNaN(num2)) {
-    return false;
-  } else {
-    let result;
-    switch (sign) {
-      case '+': {
-        result = num1 + num2;
-        break;
-      }
-      case '-': {
-        result = num1 - num2;
-        break;
-      }
-      case '*': {
-        result = num1 * num2;
-        break;
-      }
-      case '/': {
-        result = num1 / num2;
-        break;
-      }
-      default: {
-        result = "wrong sign";
-      }
-    }
-    return result;
-  } 
+
+function isAdult (age) {
+  return age >=18 ? true : false;
 }
-console.log(calc(num1, num2, sign));
 
-
-let day = 11;
-
-
-switch(day) {
-  case 1: 
-  case 2:
-  case 3: 
-  case 4:  
-  case 5: 
-  case 6:
-  case 7: 
-  case 8: 
-  case 9: 
-  case 10: {
-    console.log ('first decade');
-    break;
-  }
-  case 11: 
-  case 12:
-  case 13: 
-  case 14:  
-  case 15: 
-  case 16:
-  case 17: 
-  case 18: 
-  case 19: 
-  case 20: {
-    console.log ('second decade');
-    break;
-  }
-  case 21: 
-  case 22:
-  case 23: 
-  case 24:  
-  case 25: 
-  case 26:
-  case 27: 
-  case 28: 
-  case 29: 
-  case 30: 
-  case 31:{
-    console.log ('third decade');
-    break;
-  }
-  default : {
-    console.log ('missed');
-  }
+function checkMultiplicity (num1, num2) {
+  return  (num1 % num2 === 0) ? true : false;
 }
+
+
+// function countFactorial (num) {
+//   let result = 1;
+//   let count = 1;
+//   if (num < 0) {
+//     return false;
+//   }
+//   if(num === 0) {
+//     result = 1;
+//   }
+
+//   while (count++ <= num) {
+//     result *= count;
+//   }
+//   return result;
+// }
+
+// function countPower(num, power) {
+//   if (power === 0) {
+//     return 1;
+//   }
+//   let result = 1;
+//   if (power < 0) {
+//     for (let i = -1; i >= power; i-- ) {
+//       result *= num;
+//     }
+//     result = 1 / result;
+//   }
+
+//   for (let i = 1; i <= power; i++ ) {
+//     result *= num;
+//   }
+//   return result;
+// }
+// console.log(countPower(1, 4)); //1
+// console.log(countPower(1, 0)); //0
+// console.log(countPower(0, 4)); //0
+// console.log(countPower(2, 3)); //8
+// console.log(countPower(-2, 3)); //-8
+// console.log(countPower(2, -3)); //0.125
+// console.log(countPower(2, -1)); //0.5
