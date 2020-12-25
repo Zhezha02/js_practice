f1dfae2fb86541abccc73260246c496d55cfe55f
Evgeniya <evgeniyakirtoka@gmail.com>
2020-12-25 18:23:25 +0200

    Add math functions and function isAdult

--- index.js
+++ index.js
@@ -1,41 +1,81 @@
+function isAdult(age) {
+  return age >= 18 ? true : false;
+}
 
+function checkMultiplicity(num1, num2) {
+  return num1 % num2 === 0 ? true : false;
+}
 
-function countFactorial (num) {
-  let result = 1;
-  let count = 1;
-  if (num < 0) {
+function isTriangle(sideA, sideB, sideC) {
+  if (sideA <= 0 || sideB <= 0 || sideC <= 0) {
     return false;
   }
-  if(num === 0) {
-    result = 1;
-  }
-  while (count++ <= num) {
-    result *= count;
-  }
-  return result;
-}
-
-function countPower(num, power) {
-  if (power === 0) {
-    return 1;
-  }
-  let result = 1;
-  if (power < 0) {
-    for (let i = -1; i >= power; i-- ) {
-      result *= num;
+  const sumOfOppositeA = sideB + sideC;
+  const sumOfOppositeB = sideA + sideC;
+  const sumOfOppositeC = sideA + sideB;
+  if (sideA >= sideB && sideA >= sideC) {
+    if (sideA < sumOfOppositeA) {
+      return true;
     }
-    result = 1 / result;
+    return false;
   }
+  if (sideB >= sideA && sideB >= sideC) {
+    if (sideB < sumOfOppositeB) {
+      return true;
+    }
+    return false;
+  }
+  if (sideC >= sideA && sideC >= sideB) {
+    if (sideC < sumOfOppositeC) {
+      return true;
+    }
+    return false;
+  }
+}
 
-  for (let i = 1; i <= power; i++ ) {
-    result *= num;
+function getRhombusArea1(side, heignt) {
+  if (side <= 0 || heignt <= 0) {
+    return null;
   }
+  const result = side * heignt;
   return result;
 }
-// console.log(countPower(1, 4)); //1
-// console.log(countPower(1, 0)); //0
-// console.log(countPower(0, 4)); //0
-// console.log(countPower(2, 3)); //8
-// console.log(countPower(-2, 3)); //-8
-// console.log(countPower(2, -3)); //0.125
-// console.log(countPower(2, -1)); //0.5
+function getRhombusArea2(diagonal1, diagonal2) {
+  if (diagonal1 <= 0 || diagonal2 <= 0) {
+    return null;
+  }
+  const result = (1 / 2) * diagonal1 * diagonal2;
+  return result;
+}
+
+function getCylinderArea(radius, height) {
+  if (radius <= 0 || heignt <= 0) {
+    return null;
+  }
+  const sideArea = 2 * Math.PI * radius * height;
+  const baseArea = Math.PI * radius ** 2;
+  const result = sideArea + 2 * baseArea;
+  return result;
+}
+
+function getTriangleArea(sideA, sideB, sideC) {
+  if (sideA <= 0 || sideB <= 0 || sideC <= 0) {
+    return null;
+  }
+  const semiperimeter = (sideA + sideB + sideC) / 2;
+  const result =
+    (semiperimeter *
+      (semiperimeter - sideA) *
+      (semiperimeter - sideB) *
+      (semiperimeter - sideC)) **
+    (1 / 2);
+  return result;
+}
+
+function getRectangleArea(side1, side2) {
+  if (side1 <= 0 || side2 <= 0) {
+    return null;
+  }
+  result = side1 * side2;
+  return result;
+}
